952beaf72a8e5215000fc6703fcfa1229aac3af5
Jesco Wuester <jescowuester@gmail.com>
2019-04-02 21:14:00 +0200

    initial

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -25,20 +17,11 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <div>
+          <main className="main">{children}</main>
+          <footer className="footer">
+            © {new Date().getFullYear()}, Built by
+            <a href="https://github.com/jescowuester">@JSco</a>
           </footer>
         </div>
       </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./index.css"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO
+      title="A louer à Thoiry, Pays de Gex"
+      keywords={[`Pays de Gex`, `Thoiry`, `louer`, `habiter`, `Maison`]}
+    />
+    <div>
+      <section className="main__section">
+        <Image />
+        <div>
+          <h1>A louer à Thoiry, Pays de Gex (France):</h1>
+          <p>
+            Maison de caractère de 1850 (220m2) entiérement rénovée. Magnifique
+            jardin (1200m2) clôturé, ensoleillé et calme, belle terrasse avec
+            vue sur le Mont-Blanc et terasse couverte dans la grange attaché à
+            la maison. Grand salon/salle à manger (45 m2), cuisine equipée, 5
+            chambres, 2 salles de bains, cave voutée, garage, dépandances,
+            chauffage à gaz. Proche du bus Y, grand centre commercial, école
+            primaire au village.
+          </p>
+        </div>
+      </section>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
